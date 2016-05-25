b63b9aa908b36e2891f5791b3c61cc21677a7187
kepes.peter <kepes.peter@codeplay.hu>
2016-05-25 15:42:38 +0200

    Unicorn starting order changed
    don't run unicorn before migration

--- script/docker-entrypoint.sh
+++ script/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 export SECRET_KEY_BASE=`rake secret`
-bundle exec unicorn -c $UNICORN_CONFIG -E $RAILS_ENV -D
 bundle exec rake db:migrate
 bundle exec rails c < db/add_first_agent.rb
+bundle exec unicorn -c $UNICORN_CONFIG -E $RAILS_ENV -D
 tail -f $RAILS_LOG
